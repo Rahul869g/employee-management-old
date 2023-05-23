e6db82afa14c45edc1b9af16df95137c367373b8
adnan2212 <adnanshaikh2607@gmail.com>
2023-05-23 17:01:35 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import FirstPage from "./pages/firstPage";
+import SecondPage from "./pages/secondPage";
+import Footer from "./components/Footer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <FirstPage />,
+  },
+  {
+    path: "/projects",
+    element: <SecondPage />,
+  },
+]);
+
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={appRouter}></RouterProvider>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
